be211205635bd5b1718cbe76d16fc827bfa2cd2e
Victor Baybekov <vbaybekov@gmail.com>
2018-03-02 17:02:02 +0300

    Deploy token mocks

--- src/deploy.ts
+++ src/deploy.ts
@@ -1,6 +1,6 @@
 import { W3 } from 'soltsice';
 import { config } from './config';
-import { TokenStarsAuctionHub } from './contracts';
+import { TokenStarsAuctionHub, TokenStarsAuctionHubMock, AceToken, TeamToken } from './contracts';
 
 // throw new Error('Comment this line to deploy the auction hub');
 
@@ -8,6 +8,9 @@ const w3 = new W3(new W3.providers.HttpProvider(config.web3));
 const u = W3.EthUtils;
 const privateKey = config.privateKey.startsWith('0x') ? config.privateKey : '0x' + config.privateKey;
 const ownerAddress = u.bufferToHex(u.privateToAddress(u.toBuffer(privateKey)));
+w3.defaultAccount = ownerAddress;
+W3.default = w3;
+
 let txParams = W3.TX.txParamsDefaultDeploy(ownerAddress);
 txParams.gas = 6000000;
 txParams.gasPrice = 21000000000;
@@ -25,15 +28,27 @@ async function deploy() {
     try {
         if (!config.hubAddress) {
             console.log('DEPLOYING...');
-            let hub = await TokenStarsAuctionHub.new(txParams, {}, w3, undefined, privateKey);
-            console.log('TokenStarsAuctionHub ADDRESS: ', hub.address);
+
+            let ace = await AceToken.new(txParams, {}, w3, undefined, privateKey);
+            console.log('DEPLOYED ACE TOKEN MOCK AT: ', ace.address);
+
+            let team = await TeamToken.new(txParams, {}, w3, undefined, privateKey);
+            console.log('DEPLOYED TEAM TOKEN MOCK AT: ', team.address);
+
+            let hub = await TokenStarsAuctionHubMock.new(txParams, {_wallet: ownerAddress, _tokens: [ace.address, team.address]}, w3, undefined, privateKey);
+            console.log('HUB ADDRESS. Add this TokenStarsAuctionHub address to config/config.json to use it on backend. ', hub.address);
             let testAuctionTx = await hub.createAuction((new Date(2018, 5, 1).getTime()) / 1000, 10e18, 1, "test_auction", true, txParams, privateKey);
-            console.log('AUCTION TX: ', testAuctionTx);
-            console.log('AUCTION TX LOGS: ', testAuctionTx.logs);
+            // console.log('AUCTION TX: ', testAuctionTx);
+            // console.log('AUCTION TX LOGS: ', testAuctionTx.logs);
             let testAuctionaddress = testAuctionTx.logs![0].args!.auction;
             console.log('TEST AUCTION ADDRESS: ', testAuctionaddress);
+            console.log('Add this TokenStarsAuctionHub address to config/config.json to use it on backend: ', hub.address);
         } else {
             console.log('SKIPPING DEPLOY: Config already has the hub address');
+            let hub = await TokenStarsAuctionHub.at(config.hubAddress);
+            console.log('HUB ADDRESS: ', hub.address);
+            // let events = await hub.getLogs();
+            // console.log('EVENTS: ', events);
         }
     } catch (e) {
         console.log('Cannot deploy TokenStarsAuctionHub: ', e);
